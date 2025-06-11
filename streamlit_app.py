e116abc1d6ce3d3ca65ea05a621189ccf580c4ea
Tom Alcorn <s2517781@ed.ac.uk>
2025-06-11 23:29:01 +0100

    demo dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,23 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Finance Tracker")
 
+random_nums = [1, 2, 4, 10]
+demo_df = pd.DataFrame(random_nums, columns=["Nums"])
+
+# Editable DataFrame
+st.data_editor(
+    demo_df,
+    column_config={
+        "Nums": st.column_config.NumberColumn(
+            "Price (in GBP)",
+            help="The price of the product in GBP",
+            min_value=0,
+            max_value=1000,
+            step=1,
+            format="£%d",
+        )
+    },
+    hide_index=True,
+)
