775d79d4f9f92cd63ec88184eda4bbec962eaf05
Tom Alcorn <s2517781@ed.ac.uk>
2025-06-12 22:57:22 +0100

    repo tidying

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,21 +3,39 @@ import pandas as pd
 
 st.title("Finance Tracker")
 
-random_nums = [1, 2, 4, 10]
-demo_df = pd.DataFrame(random_nums, columns=["Nums"])
+prices = [2, 3, 4, 20]
+saved = [1, 2, 4, 10]
+
+# Compute progress dynamically
+progress = [min(s / p, 1.0) for s, p in zip(saved, prices)]  # cap at 1.0 (100%)
+
+# Create DataFrame
+demo_df = pd.DataFrame({
+    "Saved": saved,
+    "Price": prices,
+    "Progress": progress,
+})
 
 # Editable DataFrame
 st.data_editor(
     demo_df,
+    num_rows="dynamic",
     column_config={
-        "Nums": st.column_config.NumberColumn(
+        "Price": st.column_config.NumberColumn(
             "Price (in GBP)",
             help="The price of the product in GBP",
             min_value=0,
             max_value=1000,
             step=1,
             format="£%d",
-        )
+        ),
+        "Progress": st.column_config.ProgressColumn(
+            "Complete",
+            help="Amount saved as a percent of price",
+            min_value=0,
+            max_value=1,
+            format="percent",
+        ),
     },
     hide_index=True,
 )
