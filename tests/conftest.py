e6456632ae39e1a9e8ea64cd8283d24a1bee6d7c
tom.alcorn <tom.alcorn@lseg.com>
2025-12-17 22:49:37 +0000

    Refactor test fixtures to use a sample user model and support multiple sample users

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 """Helper functions and fixtures for tests."""
 
 import typing
+import uuid
 
 import pytest
 import st_supabase_connection
@@ -19,19 +20,48 @@ def _connection() -> st_supabase_connection.SupabaseConnection:
     )
 
 
+@pytest.fixture(name="sample_user")
+def _sample_user() -> backend_models.UserModel:
+    """Provide a sample user model for tests."""
+    return backend_models.UserModel(first_name="Test", last_name="User1")
+
+
 @pytest.fixture(name="yield_sample_user")
 def _yield_sample_user(
     connection: st_supabase_connection.SupabaseConnection,
+    sample_user: backend_models.UserModel,
 ) -> typing.Generator[backend_models.UserModel, None, None]:
     """Set up a sample user for tests."""
-    sample_user = backend_models.UserModel(first_name="Test", last_name="User")
-    # Insert user into the test database
     connection.table("users").insert(sample_user.model_dump(mode="json")).execute()
 
     yield sample_user
 
     # Clean up the user from the test database
     connection.table("users").delete().eq("id", sample_user.id).execute()
+
+
+@pytest.fixture(name="yield_sample_users")
+def _yield_sample_users(
+    sample_user: backend_models.UserModel,
+    connection: st_supabase_connection.SupabaseConnection,
+) -> typing.Generator[list[backend_models.UserModel], None, None]:
+    """Set up multiple sample users for tests."""
+    sample_users = [
+        sample_user,
+        sample_user.model_copy(
+            update={"id": uuid.uuid4(), "last_name": "User2"},
+            deep=True,
+        ),
+    ]
+    # Insert users into the test database
+    for user in sample_users:
+        connection.table("users").insert(user.model_dump(mode="json")).execute()
+
+    yield sample_users
+
+    # Clean up the users from the test database
+    for user in sample_users:
+        connection.table("users").delete().eq("id", user.id).execute()
 
 
 def get_rendered_texts(app_tester: st_test.AppTest) -> list[str]:
