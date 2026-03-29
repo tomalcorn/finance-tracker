934cde503a139229a3f1acc9ea76eee898016673
tom.alcorn <tom.alcorn@lseg.com>
2026-03-29 15:15:28 +0000

    added new table names

--- src/libs/dfes/constants.py
+++ src/libs/dfes/constants.py
@@ -9,3 +9,8 @@ class TableNames(enum.StrEnum):
     PAYMENTS = enum.auto()
     BANK_ACCOUNTS = enum.auto()
     BANK_ACCOUNTS_VIEW = enum.auto()
+    BUDGET_TRACKER = enum.auto()
+    EXPENSE_SOURCES = enum.auto()
+    EXPENSE_SOURCES_VIEW = enum.auto()
+    INCOME_SOURCES = enum.auto()
+    INCOME_SOURCES_VIEW = enum.auto()
