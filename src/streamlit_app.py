07431638d644aa04d39de5577fb64ef8325b8a56
tom.alcorn <tom.alcorn@lseg.com>
2026-03-29 15:15:39 +0000

    calling module functions from app file

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -2,19 +2,21 @@
 
 import streamlit as st
 
-from apps.blocks import bank_accounts_block, payments_block
+from apps.blocks import bank_accounts_block, budget_tracker_block, payments_block
 
 payments_container = st.container()
 bank_accounts_container = st.container()
-ba_block = bank_accounts_block.BankAccountsBlock()
-payments_block_instance = payments_block.PaymentsBlock()
+budget_tracker_container = st.container()
 
-
-ba_block.commit()
-payments_block_instance.commit()
+bank_accounts_block.commit()
+payments_block.commit()
+budget_tracker_block.commit()
 
 with bank_accounts_container:
-    ba_block.render()
+    bank_accounts_block.render()
 
 with payments_container:
-    payments_block_instance.render()
+    payments_block.render()
+
+with budget_tracker_container:
+    budget_tracker_block.render()
