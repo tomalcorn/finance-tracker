1e0e42a6a668a51c55c2b32267875cb434018104
tom.alcorn <tom.alcorn@lseg.com>
2026-05-04 15:23:34 +0000

    updated login to use new enum

--- src/apps/pages/login.py
+++ src/apps/pages/login.py
@@ -2,11 +2,14 @@
 
 import streamlit as st
 
+from apps.pages import constants
 from libs import auth
 
 st.title(":material/lock: Login")
 
-user = auth.get_current_user()
-
-st.success(f"Logged in as **{user.first_name} {user.last_name}**")
-st.caption("Authentication is currently stubbed with a hardcoded user.")
+if auth.is_logged_in():
+    user = auth.get_current_user()
+    st.success(f"Logged in as **{user.first_name} {user.last_name}**")
+elif st.button("Log in"):
+    auth.get_current_user()
+    st.switch_page(constants.Pages.DASHBOARD.value)
