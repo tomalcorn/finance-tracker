4f80c17cc2c5115db61b9518b523d46ee93eb67e
tom.alcorn <tom.alcorn@lseg.com>
2026-05-04 15:23:17 +0000

    move bulk app functionality into dashboard, and added constants for pages, wired into main app

---
+++ src/apps/pages/constants.py
@@ -0,0 +1,16 @@
+"""Constants for Streamlit page paths."""
+
+import enum
+
+import streamlit as st
+
+
+class Pages(enum.Enum):
+    """File paths for each page in the application."""
+
+    DASHBOARD = st.Page(
+        "apps/pages/dashboard.py",
+        title="Dashboard",
+        icon=":material/dashboard:",
+    )
+    LOGIN = st.Page("apps/pages/login.py", title="Login", icon=":material/lock:")
---
+++ src/apps/pages/dashboard.py
@@ -0,0 +1,46 @@
+"""Dashboard page for the finance tracker application."""
+
+import streamlit as st
+
+from apps.blocks import (
+    bank_accounts_block,
+    budget_tracker_block,
+    one_offs_block,
+    payments_block,
+    subscriptions_block,
+)
+from libs import subscription_reconciler
+
+one_offs_container = st.container(border=True)
+budget_tracker_container = st.container(border=True)
+payments_container = st.container(border=True)
+bank_accounts_container = st.container(border=True)
+subscriptions_container = st.container(border=True)
+
+bank_accounts_block.commit()
+payments_block.commit()
+budget_tracker_block.commit()
+one_offs_block.commit()
+subscriptions_block.commit()
+
+subscription_reconciler.SubscriptionReconciler().reconcile()
+
+with one_offs_container:
+    st.subheader(":material/bubble_chart: :violet[One-Offs]")
+    one_offs_block.render()
+
+with budget_tracker_container:
+    st.subheader(":material/pie_chart: :red[Budget Tracker]")
+    budget_tracker_block.render()
+
+with payments_container:
+    st.subheader(":material/payments: :green[Payments]")
+    payments_block.render()
+
+with bank_accounts_container:
+    st.subheader(":material/account_balance: :orange[Bank Accounts]")
+    bank_accounts_block.render()
+
+with subscriptions_container:
+    st.subheader(":material/autorenew: :blue[Subscriptions]")
+    subscriptions_block.render()
