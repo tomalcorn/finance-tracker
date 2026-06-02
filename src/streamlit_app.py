ac3c803309f0bc4b3de0f89009df95998e986d76
Tom Alcorn <s2517781@ed.ac.uk>
2026-06-02 22:16:15 +0100

    added FIRST_PASS key and updated wiring

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -3,15 +3,18 @@
 import streamlit as st
 
 from apps.pages import constants
-from libs import auth
+from libs import auth, ss_keys
 
 st.set_page_config(layout="wide")
 
 if not st.user.is_logged_in:
-    st.button("Log in", on_click=st.login, args=["auth0"])
+    st.session_state[ss_keys.SSKeys.FIRST_PASS] = True
+    st.login("auth0")
     st.stop()
 
-auth.get_current_user()
+if st.session_state[ss_keys.SSKeys.FIRST_PASS]:
+    auth.authenticate_supabase_and_seed_default_budget_trackers(auth.get_current_user())
+    st.session_state[ss_keys.SSKeys.FIRST_PASS] = False
 
 pages = st.navigation(
     [constants.Pages.DASHBOARD.value, constants.Pages.LOGIN.value],
