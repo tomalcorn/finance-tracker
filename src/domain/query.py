b5e1085751f952af50b8fd581251be4c3490a67b
Tom Alcorn <s2517781@ed.ac.uk>
2026-06-21 16:34:24 +0100

    added filters and column queries to domain

---
+++ src/domain/query.py
@@ -0,0 +1,79 @@
+"""Shared vocabulary to describe querying."""
+
+import datetime
+import enum
+from typing import Annotated
+
+import pydantic
+
+type FilterValue = str | int | float | bool | datetime.date | datetime.datetime
+type ColumnName = Annotated[
+    str,
+    pydantic.Field(description="The name of a column."),
+]
+
+
+class SortingValues(enum.StrEnum):
+    """Sorting direction values."""
+
+    ASC = enum.auto()
+    DESC = enum.auto()
+
+
+class Filters(pydantic.BaseModel):
+    """Model for a column filter."""
+
+    model_config = pydantic.ConfigDict(
+        serialize_by_alias=True,
+    )
+
+    eq: Annotated[
+        FilterValue | None,
+        pydantic.Field(description="Equality filter value."),
+    ] = None
+    in_: Annotated[
+        list[FilterValue] | None,
+        pydantic.Field(description="In filter values.", serialization_alias="in"),
+    ] = None
+    lt: Annotated[
+        FilterValue | None,
+        pydantic.Field(description="Less than filter value."),
+    ] = None
+    lte: Annotated[
+        FilterValue | None,
+        pydantic.Field(description="Less than or equal to filter value."),
+    ] = None
+    gt: Annotated[
+        FilterValue | None,
+        pydantic.Field(description="Greater than filter value."),
+    ] = None
+    gte: Annotated[
+        FilterValue | None,
+        pydantic.Field(description="Greater than or equal to filter value."),
+    ] = None
+    contains: Annotated[
+        str | None,
+        pydantic.Field(description="Contains filter value for string matching."),
+    ] = None
+    cs: Annotated[
+        str | None,
+        pydantic.Field(description="Array contains filter value."),
+    ] = None
+
+
+type OptionalFilters = Annotated[
+    Filters | None,
+    pydantic.Field(description="Optional list of filters to apply."),
+]
+type OptionalSorting = Annotated[
+    SortingValues | None,
+    pydantic.Field(description="Optional direction in which to sort the column."),
+]
+
+
+class ColumnQuery(pydantic.BaseModel):
+    """Model for querying a column."""
+
+    column_name: ColumnName
+    filters: OptionalFilters = None
+    sorting_direction: OptionalSorting = None
