4e2cae9bdf205edb0d2280c598ad82758e7b8cc3
Tom Alcorn <s2517781@ed.ac.uk>
2026-06-23 14:26:10 +0100

    tests fixed

--- tests/unit/adapters/test_client.py
+++ tests/unit/adapters/test_client.py
@@ -10,45 +10,37 @@ def test_apply_filters_to_query(
     col_configs: list[frontend_models.DFEColumnConfig],
 ) -> None:
     """Test applying filters to a query."""
-    with mock.patch(
-        "adapters.supabase.client.st_supabase_connection",
-    ) as mock_st_supabase:
-        mock_query = mock.Mock()
-        mock_st_supabase.SyncSelectRequestBuilder.return_value = mock_query
+    mock_query = mock.Mock()
 
-        client._apply_filters_to_query(
-            mock_query,
-            col_configs[0].column_name,
-            col_configs[0].filters,
-        )
+    client._apply_filters_to_query(
+        mock_query,
+        col_configs[0].column_name,
+        col_configs[0].filters,
+    )
 
-        assert all(
-            [
-                mock_query.filter.mock_calls[0][1][1] == "lte",
-                mock_query.filter.mock_calls[0][1][2] == "2023-01-01",
-                mock_query.filter.mock_calls[1][1][1] == "gte",
-                mock_query.filter.mock_calls[1][1][2] == "2022-01-01",
-            ],
-        )
+    assert all(
+        [
+            mock_query.filter.mock_calls[0][1][1] == "lte",
+            mock_query.filter.mock_calls[0][1][2] == "2023-01-01",
+            mock_query.filter.mock_calls[1][1][1] == "gte",
+            mock_query.filter.mock_calls[1][1][2] == "2022-01-01",
+        ],
+    )
 
 
 def test_apply_sorting_to_query(
     col_configs: list[frontend_models.DFEColumnConfig],
 ) -> None:
     """Test applying sorting to a query."""
-    with mock.patch(
-        "adapters.supabase.client.st_supabase_connection",
-    ) as mock_st_supabase:
-        mock_query = mock.Mock()
-        mock_st_supabase.SyncSelectRequestBuilder.return_value = mock_query
+    mock_query = mock.Mock()
 
-        client._apply_sorting_to_query(
-            mock_query,
-            col_configs[0].column_name,
-            col_configs[0].sorting,
-        )
+    client._apply_sorting_to_query(
+        mock_query,
+        col_configs[0].column_name,
+        col_configs[0].sorting,
+    )
 
-        mock_query.order.assert_called_once_with(
-            "col1",
-            desc=False,
-        )
+    mock_query.order.assert_called_once_with(
+        "col1",
+        desc=False,
+    )
